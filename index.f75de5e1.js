e9aff6cb39c3e5b6407d1220e86866b6908e84b1
Alla Serhiienko <allasergienko706@gmail.com>
2024-11-29 11:43:59 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,34 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+console.log("Script is working!");
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    const currentGoods = Object.entries(data);
+    if (currentGoods.length === 0) return;
+    for (const [key, value] of currentGoods){
+        const listItem = document.createElement("li");
+        listItem.textContent = key;
+        list.append(listItem);
+        if (Object.keys(value).length > 0) createTree(listItem, value);
+    }
+    element.append(list);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
